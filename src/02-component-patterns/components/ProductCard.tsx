6b52359730342f9de02e3f2800d06cb0c8d0fb27
José Enrique <otipip90@gmail.com>
2021-11-26 23:49:58 -0500

    created product component has a simple way architecture

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,23 +1,26 @@
+import { useProduct } from '../hooks/useProduct';
 import styles from '../styles/styles.module.css';
 import noImage from '../assets/no-image.jpg';
-import { useState } from 'react';
 
-export const ProductCard = () => {
-  const [counter, setCounter] = useState(0);
+interface Props {
+  product: Product;
+}
+interface Product {
+  id: string;
+  title: string;
+  img?: string;
+}
 
-  const increaseBy = (value: number) => {
-    setCounter((prev) => Math.max(prev + value, 0));
-  };
-
+export const ProductCard = ({ product }: Props) => {
+  const { counter, increaseBy } = useProduct();
   return (
     <div className={styles.productCard}>
       <img
         className={styles.productImg}
-        src='./coffee-mug.png'
+        src={product.img ? product.img : noImage}
         alt='Cofee Mug'
       />
-      {/* <img className={styles.productImg} src={noImage} alt='No image' /> */}
-      <span className={styles.productDescription}>Cofee Mug</span>
+      <span className={styles.productDescription}>{product.title}</span>
       <div className={styles.buttonsContainer}>
         <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>
           -
--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { ProductCard } from '../components/ProductCard';
 
+const product = {
+  id: '',
+  title: 'Cofee Mug - Card',
+  img: './coffee-mug.png',
+};
 export const ShoppingPage = () => {
   return (
     <div>
       <h1>Shopping Store</h1>
       <hr />
-      <ProductCard />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+        }}
+      >
+        <ProductCard product={product} />
+      </div>
     </div>
   );
 };
