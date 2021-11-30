0c7d4699fba8429bc54bcdfe3bb5886cf1ea8350
José Enrique <otipip90@gmail.com>
2021-11-30 00:31:59 -0500

    Merge branch 'component-extend-styles' of https://github.com/joseenrique-dev/react-adv into component-extend-styles
    # Conflicts:
    #	src/02-component-patterns/components/ProductButtons.tsx
    #	src/02-component-patterns/components/ProductCard.tsx
    #	src/02-component-patterns/components/ProductImage.tsx
    #	src/02-component-patterns/components/ProductTitle.tsx
    #	src/02-component-patterns/components/index.ts
    #	src/02-component-patterns/hooks/useProduct.ts
    #	src/02-component-patterns/interfaces/interfaces.ts
    #	src/02-component-patterns/pages/ShoppingPage.tsx
    #	src/02-component-patterns/styles/custom-styles.css
    #	src/routes/Navigation.tsx

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -4,12 +4,19 @@ import {
   ProductImage,
   ProductTitle,
 } from '../components';
+import { Product } from '../interfaces/interfaces';
 import '../styles/custom-styles.css';
-const product = {
+const product1 = {
   id: '',
   title: 'Cofee Mug - Card',
   img: './coffee-mug.png',
 };
+const product2 = {
+  id: '',
+  title: 'Cofee Mug - Meme',
+  img: './coffee-mug2.png',
+};
+const products: Product[] = [product1, product2];
 export const ShoppingPage = () => {
   return (
     <div>
@@ -22,29 +29,45 @@ export const ShoppingPage = () => {
           flexWrap: 'wrap',
         }}
       >
-        <ProductCard product={product} className='bg-dark'>
-          <ProductCard.Image className='custom-image' />
-          <ProductCard.Title className='text-white text-bold' />
-          <ProductCard.Buttons className='custom-buttons' />
-        </ProductCard>
-
-        <ProductCard product={product} className='bg-dark'>
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            className='bg-dark text-white'
+          >
+            <ProductImage
+              style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}
+              className='bg-dark text-bold'
+            />
+            <ProductTitle style={{ fontWeight: 'bold' }} />
+            <ProductButton className='custom-buttons' />
+          </ProductCard>
+        ))}
+      </div>
+      <div className='shopping-card'>
+        <ProductCard
+          key={product2.id}
+          product={product2}
+          className='bg-dark text-white'
+          style={{ width: '100px' }}
+        >
           <ProductImage
-            className='custom-image'
             style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}
+            className='bg-dark text-bold'
           />
-          <ProductTitle title={'Coffee'} className='text-white text-bold' />
           <ProductButton className='custom-buttons' />
         </ProductCard>
-
-        <ProductCard product={product} style={{ backgroundColor: '#70D1F8' }}>
+        <ProductCard
+          key={product1.id}
+          product={product1}
+          className='bg-dark text-white'
+          style={{ width: '100px' }}
+        >
           <ProductImage
             style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}
+            className='bg-dark text-bold'
           />
-          <ProductTitle style={{ fontWeight: 'bold' }} />
-          <ProductButton
-            style={{ display: 'flex', justifyContent: 'flex-end' }}
-          />
+          <ProductButton className='custom-buttons' />
         </ProductCard>
       </div>
     </div>
