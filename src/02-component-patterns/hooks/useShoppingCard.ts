fc8f609f6e74e1bc34aaee8952560acc60f35d91
José Enrique <otipip90@gmail.com>
2021-11-30 23:54:07 -0500

    branch adjutsments

--- src/02-component-patterns/hooks/useShoppingCard.ts
+++ src/02-component-patterns/hooks/useShoppingCard.ts
@@ -16,31 +16,18 @@ export const useShoppingCard = () => {
     // console.log('onProductCountChange', count, product);
 
     setShoppingCart((oldShoppingCart) => {
-      const productInCart: ProductInCart = oldShoppingCart[product.id] || {
-        ...product,
-        count: 0,
-      };
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-        return {
-          ...oldShoppingCart,
-          [product.id]: productInCart,
-        };
+      if (count === 0) {
+        const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+        console.log('REMOVE', [toDelete, rest]);
+        return rest;
       }
-      const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-      return rest;
-      // if (count === 0) {
-      //   const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-      //   console.log('REMOVE', [toDelete, rest]);
-      //   return rest;
-      // }
-      // return {
-      //   ...oldShoppingCart,
-      //   [product.id]: {
-      //     ...product,
-      //     count,
-      //   },
-      // };
+      return {
+        ...oldShoppingCart,
+        [product.id]: {
+          ...product,
+          count,
+        },
+      };
     });
   };
 
