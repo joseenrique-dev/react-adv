5b205ff73101c61c1ebbf9b56ca36059c3f10ccd
José Enrique <otipip90@gmail.com>
2021-12-01 21:45:26 -0500

    clean code

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -5,7 +5,6 @@ import {
   ProductTitle,
 } from '../components';
 import { products } from '../data/products';
-import '../styles/custom-styles.css';
 
 export const ShoppingPage = () => {
   const product = products[0];
@@ -16,7 +15,6 @@ export const ShoppingPage = () => {
       <ProductCard
         key={product.id}
         product={product}
-        className='bg-dark text-white'
         initialValues={{
           count: 6,
           // maxCount: 10,
@@ -28,18 +26,8 @@ export const ShoppingPage = () => {
               className='custom-image'
               style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}
             />
-            <ProductTitle className='text-bold' />
-            <ProductButton className='custom-buttons' />
-
-            <button onClick={reset}>Reset</button>
-            <button onClick={() => increaseBy(-2)}> -2 </button>
-            {!isMaxCountReached && (
-              <button onClick={() => increaseBy(+2)}> +2 </button>
-            )}
-
-            <span>
-              {count} - {maxCount}
-            </span>
+            <ProductTitle />
+            <ProductButton />
           </>
         )}
       </ProductCard>
