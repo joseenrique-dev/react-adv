d93b279a96f6e10c96bfc1bc696a0d0b90c14499
José Enrique <otipip90@gmail.com>
2021-12-02 14:49:47 -0500

    validate form

--- src/03-forms/hooks/useForm.ts
+++ src/03-forms/hooks/useForm.ts
@@ -12,9 +12,21 @@ export const useForm = <T>(initialState: T) => {
     });
   };
 
+  const isValidEmail = (email: string) => {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  };
+
+  const resetForm = () => {
+    setFormData({ ...initialState });
+  };
+
   return {
     ...formData,
     formData,
+    isValidEmail,
     onChange,
+    resetForm,
   };
 };
--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -3,7 +3,16 @@ import { useForm } from '../hooks/useForm';
 import '../styles/styles.css';
 
 export const RegisterPage = () => {
-  const { name, email, password, password2, formData, onChange } = useForm({
+  const {
+    name,
+    email,
+    password,
+    password2,
+    formData,
+    onChange,
+    resetForm,
+    isValidEmail,
+  } = useForm({
     name: '',
     email: '',
     password: '',
@@ -24,14 +33,18 @@ export const RegisterPage = () => {
           placeholder='Name'
           value={name}
           onChange={onChange}
+          className={`${name.trim().length <= 0 && 'class-error'}`}
         />
+        {name.trim().length <= 0 && <span>This field is required</span>}
         <input
           type='email'
           name='email'
           placeholder='Email'
           value={email}
           onChange={onChange}
+          className={`${!isValidEmail(email) && 'class-error'}`}
         />
+        {!isValidEmail(email) && <span>Invalid Email</span>}
         <input
           type='password'
           name='password'
@@ -39,6 +52,10 @@ export const RegisterPage = () => {
           value={password}
           onChange={onChange}
         />
+        {password.trim().length <= 0 && <span>This field is required</span>}
+        {password.trim().length < 6 && password.trim().length > 0 && (
+          <span>Should be greather than 6 characters</span>
+        )}
         <input
           type='password'
           name='password2'
@@ -46,7 +63,12 @@ export const RegisterPage = () => {
           value={password2}
           onChange={onChange}
         />
+        {password2.trim().length <= 0 && <span>This field is required</span>}
+        {password2.trim().length > 0 && password !== password2 && (
+          <span>You should write the same password.</span>
+        )}
         <button type='submit'>Send</button>
+        <button onClick={resetForm}>Reset Form</button>
       </form>
     </div>
   );
